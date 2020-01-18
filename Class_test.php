7245463bd8678b467a962d61da5c2a6c6404fd3e
Alexandre <bighal.cormier@gmail.com>
2020-01-18 21:15:18 +0100

    Create Class_test.php

---
+++ Class_test.php
@@ -0,0 +1,20 @@
+<?php require "LAMUSEE_Class.php"; ?>
+<!DOCTYPE html>
+<html>
+<body>
+<?php 
+
+echo "Class test";
+
+
+$lm; 
+$lm = new Lamusee();
+
+$lm->create_tables();
+
+
+?>
+
+
+</body>
+</html>
